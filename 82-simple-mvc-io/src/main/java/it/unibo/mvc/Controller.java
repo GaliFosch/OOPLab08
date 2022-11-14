fbd4ca4b007dad6d6541354183813cca398a7a41
Galileo Foschini - galileo.foschini@studio.unibo.it <galileo.foschini@studio.unibo.it>
2022-11-14 12:34:29 +0100

    implemented Controller

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private final static String PATH = System.getProperty("user.home")
+            +System.getProperty("file.separator")
+            +"output.txt";
+    private File file;
+
+    public Controller() {
+        this.file = new File(PATH);
+    }
+
+    public void printString(final String string) throws IOException{
+        try ( PrintStream ps = new PrintStream(file) ) {
+            ps.println(string);
+        } catch (IOException e){
+            throw e;
+        }
+    }
+
+    public void setFile(final File file) {
+        this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getPath(){
+        return file.getPath();
+    }
+
+    
 }
