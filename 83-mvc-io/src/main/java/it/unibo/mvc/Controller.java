8882b3b61ca46f59b2a077890d6e148f90e9eee8
Galileo <gali22fosch@gmail.com>
2022-11-19 19:01:41 +0100

    created and implemented Controller interface

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,17 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    void setNextString(final String string);
+
+    String getNexString();
+
+    List<String> getHistory();
+    
+    void printString() throws IllegalStateException;
 }
--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,42 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String nexString;
+    private ArrayList<String> history;
+
+    @Override
+    public void setNextString(final String string) {
+        this.nexString = string;
+    }
+
+    @Override
+    public String getNexString() {
+        return String.valueOf(nexString);
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return List.copyOf(history);
+    }
+
+    @Override
+    public void printString() throws IllegalStateException {
+        try{
+            Objects.requireNonNull(nexString);
+        }catch (NullPointerException e){
+            throw new IllegalStateException();
+        }
+        System.out.println(nexString);
+        history.add(nexString);
+    }
 
 }
